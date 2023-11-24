7b6c1d9519c534bea495d71e67d1dfbf197cd556
unimat <unimat@unimat.hu>
2023-11-24 12:56:12 +0100

    Add a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">                           
+    <html lang="en" className={inter.variable}>                           
       <body className={inter.variable}>
         <Theme accentColor="purple" panelBackground="solid">
           <NavBar />
