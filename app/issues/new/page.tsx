3d41905fcbac9d449c2e83144b066bedcec2cda7
unimat <unimat@unimat.hu>
2023-11-25 12:49:09 +0100

    Move Inline Function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -24,6 +24,17 @@ const NewIssuePage = () => {
   const [error, setError] =  useState('')
   const [isSubmitting, setSubmitting] = useState(false)
 
+  const onSubmit = handleSubmit( async (data) => {
+    try {
+      setSubmitting(true)
+      await axios.post('/api/issues', data)
+      router.push('/issues')  
+    } catch (error) {
+      setSubmitting(false)
+      setError('An unexpected error occured')
+    }
+  })
+
   return (
     <div className='max-w-xl'>
       { error && <Callout.Root color="red" className='mb-5'>
@@ -32,16 +43,7 @@ const NewIssuePage = () => {
       }
       <form 
         className='space-y-3' 
-        onSubmit={handleSubmit( async (data) => {
-          try {
-            setSubmitting(true)
-            await axios.post('/api/issues', data)
-            router.push('/issues')  
-          } catch (error) {
-            setSubmitting(false)
-            setError('An unexpected error occured')
-          }
-        })}>
+        onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
