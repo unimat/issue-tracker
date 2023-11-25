f5883ad82a72b04b5ed58d92d985efa73934312a
unimat <unimat@unimat.hu>
2023-11-25 16:15:03 +0100

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -9,7 +9,7 @@ import IssueActions from './IssueActions'
 const IssuesPage = async () => {
   
   const issues = await prisma.issue.findMany()
-  await delay(4000)
+  // await delay(2000)
 
   return (
     <div>
@@ -31,7 +31,9 @@ const IssuesPage = async () => {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
                 <div className='block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
