22189f8a17cab597704e5c419aa413fc89053213
unimat <unimat@unimat.hu>
2023-11-26 13:50:10 +0100

    Refactor: Organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as  Link } from './Link'
+export { default as  ErrorMessages } from './ErrorMessage'
+export { default as  IssueStatusBadge } from './IssueStatusBadge'
+export { default as  Spinner } from './Spinner'
+export { default as Skeleton } from './Skeleton'
