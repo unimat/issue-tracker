7411a011356e232ab884f7376d455dbdca5a2d8a
unimat <unimat@unimat.hu>
2023-11-26 16:43:41 +0100

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,7 +8,7 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchemas';
+import { IssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
@@ -19,7 +19,7 @@ const SimpleMDE = dynamic(
   { ssr: false }
 )
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof IssueSchema>
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter()
@@ -29,7 +29,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     control, 
     handleSubmit, 
     formState: { errors }} =useForm<IssueFormData> ({
-    resolver: zodResolver(createIssueSchema)
+    resolver: zodResolver(IssueSchema)
   })
 
   const [error, setError] =  useState('')
