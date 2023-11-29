c907012d341437784839fdf8e3a90fcc04e42225
unimat <unimat@unimat.hu>
2023-11-29 13:46:16 +0100

    Add a delete button

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,23 +1,18 @@
 'use client'
 
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { IssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Issue } from '@prisma/client';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
-import  { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import dynamic from 'next/dynamic'
-import { useForm, Controller } from 'react-hook-form'
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { IssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod'
-import ErrorMessage from '@/app/components/ErrorMessage';
-import Spinner from '@/app/components/Spinner';
-import { Issue } from '@prisma/client';
-
-const SimpleMDE = dynamic(
-  () => import('react-simplemde-editor'),
-  { ssr: false }
-)
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
 
 type IssueFormData = z.infer<typeof IssueSchema>
 
