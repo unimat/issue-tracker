2fbf18e31e57a9dc8dc2fa462d97a3c14bd3a43d
unimat <unimat@unimat.hu>
2023-11-29 10:48:40 +0100

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,9 +36,13 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const [isSubmitting, setSubmitting] = useState(false)
 
   const onSubmit = handleSubmit( async (data) => {
+    console.log(data)
     try {
       setSubmitting(true)
-      await axios.post('/api/issues', data)
+      if (issue)
+        await axios.patch('/api/issues/' + issue.id, data)
+      else
+        await axios.post('/api/issues', data)
       router.push('/issues')  
     } catch (error) {
       setSubmitting(false)
@@ -71,7 +75,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           {errors.description?.message}
         </ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
