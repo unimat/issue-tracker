bdde0e2790d0f9374015a0d94b3c5da1a4be15be
unimat <unimat@unimat.hu>
2023-12-02 16:12:41 +0100

    Enhanced the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,9 +1,9 @@
-
-import { IssueSchema } from "@/app/validationSchemas";
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
 import autOptions from "@/app/auth/authOptions";
+import { patchIssueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
 
 export async function PATCH(
   request: NextRequest, 
@@ -14,9 +14,18 @@ export async function PATCH(
       return NextResponse.json({}, {status: 401})
 
     const body = await request.json()
-    const validation = IssueSchema.safeParse(body)
+    const validation = patchIssueSchema.safeParse(body)
     if (!validation.success)
       return NextResponse.json(validation.error.format(), {status: 400});
+
+    const {assignedToUserId, title, description} = body
+
+    if (assignedToUserId) {
+      const user = await prisma.user.findUnique({ 
+        where: { id: assignedToUserId }})
+      if (!user)
+        return NextResponse.json({error: 'Invalid user.'}, {status: 400})
+    }
 
     const issue = await prisma.issue.findUnique({
       where: { id: parseInt(params.id) }
@@ -27,8 +36,9 @@ export async function PATCH(
     const UpdatedIssue = await prisma.issue.update({
       where: { id: issue.id },
       data: {
-        title: body.title,
-        description: body.description
+        title,
+        description,
+        assignedToUserId
       }
     })
 
