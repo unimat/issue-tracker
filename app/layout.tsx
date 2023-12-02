02eec42ebbfa554a8c5845a41e25f0b9c6ca90ef
unimat <unimat@unimat.hu>
2023-12-02 12:26:48 +0100

    Set up React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import NavBar from './NavBar';
 import AuthProvider from './auth/Provider';
 import './globals.css';
 import './theme-config.css';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({
    subsets: ['latin'],
@@ -25,16 +26,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>                           
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="violet" panelBackground="solid">
-            <NavBar />
-            <main className='p-5'>
-              <Container>
-                {children}
-              </Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="violet" panelBackground="solid">
+              <NavBar />
+              <main className='p-5'>
+                <Container>
+                  {children}
+                </Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
