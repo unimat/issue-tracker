97c0527632f4b3bf39c0cdbe96b5ea21ee872d50
unimat <unimat@unimat.hu>
2023-12-03 16:21:32 +0100

    Filter issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const statuses: { label: string; value?: Status }[] = [
@@ -12,14 +13,19 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter()
+
   return (
-    <Select.Root>
+    <Select.Root onValueChange={(status) => {
+      const query = status ==='ALL' ? '' : `?status=${status}`
+      router.push('/issues/list' + query)
+    } }>
       <Select.Trigger placeholder='Filter by status...' />
       <Select.Content>
       {statuses.map((status) => (
           <Select.Item
-            key={status.value}
-            value={status.value || 'x'}
+            key={status.value ?? 'ALL'}
+            value={status.value ?? 'ALL'}
           >
             {status.label}
           </Select.Item>
