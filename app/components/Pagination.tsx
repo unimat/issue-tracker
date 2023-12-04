51d188926b045e1fb8028c5bd0421326b341e011
unimat <unimat@unimat.hu>
2023-12-04 10:59:02 +0100

    Implementing pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   itemCount: number;
@@ -12,22 +15,51 @@ const Pagination = ({
   pageSize,
   currentPage,
 }: Props) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const pageCount = Math.ceil(itemCount / pageSize)
   if (pageCount <= 1) return null
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', page.toString())
+    router.push('?' + params.toString())
+  }
 
   return (
     <Flex align='center' gap='2'>
       <Text size='2'>Page {currentPage} of {pageCount}</Text>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button 
+        color="gray" 
+        variant="soft" 
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+        >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button 
+        color="gray" 
+        variant="soft" 
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+        >
         <ChevronLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button 
+        color="gray" 
+        variant="soft" 
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+        >
         <ChevronRightIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button 
+        color="gray" 
+        variant="soft" 
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+        >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
-import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export default function Home({ searchParams }: {searchParams: { page: string}}) {
   return (
     <main className="overflow-hidden">
-      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
     </main>
   )
 }
